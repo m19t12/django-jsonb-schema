14c36ec52547a7778f854586c0852a4e97d24ce5
mts <mts@geotagaeroview.com>
2017-12-15 14:43:31 +0200

    create class for adding abstract in schema models

--- django_json_schema_demo/json_schema_app/schemas.py
+++ django_json_schema_demo/json_schema_app/schemas.py
@@ -1,30 +1,24 @@
 # coding=utf-8
 from django.db import models
 
-from json_schema.models import SchemaForeignKey
+from json_schema.models import SchemaForeignKey, SchemaMeta
 
 
 class SchoolSchema(models.Model):
     name = models.CharField(max_length=128)
     address = models.CharField(max_length=128)
-
-    class Meta:
-        abstract = True
+    Meta = SchemaMeta()
 
 
 class SonSchema(models.Model):
     name = models.CharField(max_length=128)
     age = models.IntegerField()
     school = SchemaForeignKey(SchoolSchema)
-
-    class Meta:
-        abstract = True
+    Meta = SchemaMeta()
 
 
 class ParentSchema(models.Model):
     name = models.CharField(max_length=128, default="test")
     age = models.IntegerField()
     son = SchemaForeignKey(SonSchema)
-
-    class Meta:
-        abstract = True
+    Meta = SchemaMeta()
--- json_schema/models.py
+++ json_schema/models.py
@@ -2,6 +2,10 @@
 from django.db.models import ForeignKey
 
 
+class SchemaMeta:
+    abstract = True
+
+
 class SchemaForeignKey(ForeignKey):
     def validate(self, value, model_instance):
         pass
